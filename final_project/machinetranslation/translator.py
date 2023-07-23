58d17dc0d8897af94e7c69da598e2b0100020e80
w4w3ru <waweruj@protonmail.com>
2023-07-23 11:27:14 -0400

    chore: flask checkpoint

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -4,8 +4,8 @@ translator.py
 This module provides functions for translating text between English and French.
 
 Functions:
-- englishToFrench(englishText): Translates English text to French.
-- frenchToEnglish(frenchText): Translates French text to English.
+- english_to_french(english_text): Translates English text to French.
+- french_to_english(french_text): Translates French text to English.
 """
 from deep_translator import MyMemoryTranslator
 
@@ -14,7 +14,7 @@ def english_to_french(english_text):
     Translates English text to French.
 
     Parameters:
-        englishText (str): The English text to be translated.
+        english_text (str): The English text to be translated.
 
     Returns:
         str: The translated French text.
@@ -27,7 +27,7 @@ def french_to_english(french_text):
     Translates French text to English.
 
     Parameters:
-        frenchText (str): The French text to be translated.
+        french_text (str): The French text to be translated.
 
     Returns:
         str: The translated English text.
--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,26 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
+import os
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    translated_text = translator.english_to_french(textToTranslate)
+    return translated_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    translated_text = translator.french_to_english(textToTranslate)
+    return translated_text
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    template_path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+    return render_template(template_path)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
